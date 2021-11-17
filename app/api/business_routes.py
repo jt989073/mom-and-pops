297b5f093153cb7afb655c19465ce1317776fe85
James Thompson <thompsonjames0120@gmail.com>
2021-11-16 19:57:22 -0600

    get and put routes working on backend

--- app/api/business_routes.py
+++ app/api/business_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from flask_login import login_required
+from app.forms.edit_business_form import EditBusinessForm
 from app.models import db, Business
 from app.forms.business_form import BusinessForm
 
@@ -17,6 +18,26 @@ def businesses():
     return {business.id: business.to_card_dict() for business in Business.query.all()}
 
 
-@business_routes.route('/<int:id>', methods=['GET', 'PUT'])
-def updateBusiness():
-    form
+
+@business_routes.route('/<int:id>')
+def business(id):
+    business = Business.query.get(id)
+    return business.to_dict()
+
+
+@business_routes.route('/<int:id>', methods=['PUT'])
+def updateBusiness(id):
+    form = EditBusinessForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        business = Business.query.get(id)
+        business.name = form.data['name']
+        business.street = form.data['street']
+        business.city = form.data['city']
+        business.state = form.data['state']
+        business.image = form.data['image']
+        db.session.commit()
+        return business.to_dict()
+    else:
+        return form.errors
+
